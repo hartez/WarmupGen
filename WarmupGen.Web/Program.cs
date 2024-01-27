831a5b31c3b06d784845090b59a69567a5e3e71b
E.Z. Hart <hartez@gmail.com>
2024-01-27 16:10:09 -0700

    Publishing workflow stuff

--- WarmupGen.Web/Program.cs
+++ WarmupGen.Web/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 builder.Services.AddRazorPages();
