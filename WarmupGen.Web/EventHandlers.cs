8a63df7f1900e5c887b59376b243597d4c390472
E.Z. Hart <hartez@gmail.com>
2024-02-09 14:19:53 -0700

    Add fade out on delete, expand/contract settings

---
+++ WarmupGen.Web/EventHandlers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WarmupGen.Web
+{
+	[EventHandler("oncustomtransitionend", typeof(CustomTransitionEndEventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+	public static class EventHandlers
+	{
+	}
+
+	public class CustomTransitionEndEventArgs : EventArgs
+    {
+        public string? PropertyName { get; set; }
+    }
+}
