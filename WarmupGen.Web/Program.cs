b135d256ae5ff81bd9b1bda7ae43067f745b3f74
E.Z. Hart <hartez@gmail.com>
2024-02-12 13:42:41 -0700

    Turn on errors to debug prod issue

--- WarmupGen.Web/Program.cs
+++ WarmupGen.Web/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazoredModal();
 
+builder.Services.AddRazorComponents(options => 
+    options.DetailedErrors = true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
